083ea7e93fb053a0fa8d9091ec88eab8c09e813e
edwin victor <edwinvictor84@gmail.com>
2024-08-19 09:26:59 +0200

    formatted functions in inference modules to comply with flake8 standards

--- house_prices/inference.py
+++ house_prices/inference.py
@@ -1,15 +1,9 @@
 import pandas as pd
-import numpy as np
 import joblib
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import mean_squared_log_error
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import OneHotEncoder
-from IPython.display import display
+from typing import List
 
-def load_model_and_transformers(model_path: str, scaler_path: str, encoder_path: str):
+
+def load_models(model_path: str, scaler_path: str, encoder_path: str):
     """
     Loads the pre-trained model, scaler, and encoder.
 
@@ -28,27 +22,44 @@ def load_model_and_transformers(model_path: str, scaler_path: str, encoder_path:
     encoder = joblib.load(encoder_path)
     return model, scaler, encoder
 
-def preprocess_data_and_predict(Testing_df: pd.DataFrame, scaler, encoder, model, continuos_features: list, categorical_features: list) -> pd.DataFrame:
+
+def preprocess_data_and_predict(
+    Testing_df: pd.DataFrame,
+    scaler,
+    encoder,
+    model,
+    continuos_features: List[str],
+    categorical_features: List[str]
+) -> pd.DataFrame:
     """
-    Preprocesses the input data by scaling continuous features and encoding categorical features.
+    Preprocesses the input data by scaling and encoding features, and
+    then makes predictions using the provided model.
 
     Args:
-        input_data (pd.DataFrame): The data to preprocess.
+        Testing_df (pd.DataFrame): The data to preprocess.
         scaler: Scaler object to scale continuous features.
         encoder: Encoder object to encode categorical features.
-        continuous_features (list): List of continuous feature names.
-        discrete_features (list): List of discrete feature names.
+        continuos_features (List[str]): List of continuous feature names.
+        categorical_features (List[str]): List of categorical feature names.
 
     Returns:
-        pd.DataFrame: Preprocessed data.
+        pd.DataFrame: Predicted house prices.
     """
     test_scaled = scaler.transform(Testing_df[continuos_features])
     test_encoded = encoder.transform(Testing_df[categorical_features])
 
-    test_scaled_df = pd.DataFrame(test_scaled, columns=continuos_features)
-    test_encoded_df = pd.DataFrame(test_encoded, columns=encoder.get_feature_names_out(categorical_features))
+    test_scaled_df = pd.DataFrame(
+        test_scaled, columns=continuos_features
+    )
+    test_encoded_df = pd.DataFrame(
+        test_encoded,
+        columns=encoder.get_feature_names_out(categorical_features)
+    )
 
-    transformed_test_df = pd.concat([test_scaled_df, test_encoded_df], axis=1)
+    transformed_test_df = pd.concat(
+        [test_scaled_df, test_encoded_df], axis=1
+    )
     predict_house_price = model.predict(transformed_test_df)
-    return predict_house_price
-
+    return pd.DataFrame(
+        predict_house_price, columns=['PredictedPrice']
+    )
